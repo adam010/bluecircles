ca2a78d4fca5c27fcc9df0591583b45f93bf8cfb
Adam AbuAbdullah <info@blue-circles.nl>
2016-07-16 19:01:01 +0200

    views commit

--- app/controllers/loginController.php
+++ app/controllers/loginController.php
@@ -3,7 +3,8 @@
 class LoginController extends \Phalcon\Mvc\Controller{
 
    public function initialize(){
-   	echo "*** Init";
+     
+     $this->view->setTemplateAfter('default')
    }
    public function indexAction()
    {
@@ -12,11 +13,11 @@ class LoginController extends \Phalcon\Mvc\Controller{
 
    }	
    public function processAction($username = false, $age =12){
-   	  echo "Processing";
-   	  echo $username;
-   	  echo $age;
-        $this->view->setVar('username','username');
-        $this->view->setVar('username','username');
+ 
+        $this->view->setVar('username',$username);
+        $this->view->setVar('age',$age);
+        
+        $this->view->disable();
    }
   
 }
